44a01e1d6bc4de32dcbf63d44edb205d4cd6f2d3
polcio1324 <Komputer991122@wp.pl>
2019-03-26 15:37:09 +0100

    1 zajecia z js

---
+++ calc.js
@@ -0,0 +1,12 @@
+const init = () =>{
+    const c = document.getElementById('container');
+  // [...c.children].forEach(el => console.log(el)); to samo
+    };
+
+    for (let i=0;i<c.children.length;i++)
+    {
+        console.log(c.children[i]);
+    }
+
+
+window.addEventListener('DOMContentLoaded',init);
